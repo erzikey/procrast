9e7316c01eac447f0e24cd6d0ac504b8a2fe2363
erzikey <107721748+erzikey@users.noreply.github.com>
2022-07-04 15:42:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let phrases = [
+  "отправить Тёме смешную гифку",
+  "посмотреть скидки на авиабилеты",
+  "разобраться, о чём поют рэперы",
+  "Артем - долбоёб",
+  "расставить книги на полке по цвету",
+  "читать про зарплаты в Сан-Франциско",
+];
+
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
+
+let button = document.querySelector(".button");
+let phrase = document.querySelector(".phrase");
+let advice = document.querySelector(".advice");
+let image = document.querySelector(".image");
+
+button.addEventListener("click", function () {
+  phrase.textContent = getRandomElement(phrases);
+});
